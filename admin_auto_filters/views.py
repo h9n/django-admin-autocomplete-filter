0c4a3ed87e96bfb4f3dbb5e992294ac4138a2349
Hamish Brown <h@h9n.co>
2019-12-18 19:12:36 +1100

    Removed import of deprecated render_to_response

--- admin_auto_filters/views.py
+++ admin_auto_filters/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.views.generic import CreateView
 from testing.models import *
 from testing.forms import *
